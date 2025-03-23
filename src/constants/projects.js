abd637bdd1c3789156a7136c48169bf3586f52e0
Subham Mallick <chemsubham@gmail.com>
2025-03-23 20:34:47 +0530

    some Projects add and images

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -6,38 +6,25 @@ export const projects = [
       "This is a responsive GoDaddy-inspired landing page designed using Tailwind CSS",
     view: "https://subham-web98.github.io/GoDaddy-UI/",
   },
+
+  {
+    title: "Digital Marketing Course Website",
+    image: "/digital_marketing_course.png",
+    description:
+      "Designed and developed a fully responsive landing page for a digital marketing course using the Bootstrap framework, ensuring a seamless user experience across all devices.",
+    view: "https://subham-web98.github.io/Digital-Marketing-Course-/",
+  },
+  {
+    title: "Company Website UI design",
+    image: "/company_landing_page.png",
+    description:
+      "A fully responsive landing page designed for a company using Bootstrap.",
+    view: "https://subham-web98.github.io/Nebula-bootstrap/",
+  },
   {
     title: "Random Color Generator",
     image: "/random_color_generator.png",
     description: "A Random Color Generator Using HTML,CSS and Javascript",
     view: "https://subham-web98.github.io/Random_Color_Generator/",
-  },
-  {
-    title: "GoDaddy UI Landing Page",
-    image: "/godaddy_ui.png",
-    description:
-      "This is a responsive GoDaddy-inspired landing page designed using Tailwind CSS",
-    view: "https://subham-web98.github.io/GoDaddy-UI/",
-  },
-  {
-    title: "GoDaddy UI Landing Page",
-    image: "/godaddy_ui.png",
-    description:
-      "This is a responsive GoDaddy-inspired landing page designed using Tailwind CSS",
-    view: "https://subham-web98.github.io/GoDaddy-UI/",
-  },
-  {
-    title: "GoDaddy UI Landing Page",
-    image: "/godaddy_ui.png",
-    description:
-      "This is a responsive GoDaddy-inspired landing page designed using Tailwind CSS",
-    view: "https://subham-web98.github.io/GoDaddy-UI/",
-  },
-  {
-    title: "GoDaddy UI Landing Page",
-    image: "/godaddy_ui.png",
-    description:
-      "This is a responsive GoDaddy-inspired landing page designed using Tailwind CSS",
-    view: "https://subham-web98.github.io/GoDaddy-UI/",
   },
 ];
