518fedd523a3d4c88e23e34acd815159977e3c0b
Subham Mallick <chemsubham@gmail.com>
2025-04-18 00:40:42 +0530

    projects add

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,10 +1,18 @@
 export const projects = [
   {
-    title: "GoDaddy UI Landing Page",
-    image: "/godaddy_ui.png",
+    title: "CRUD App with MERN Stack",
+    image: "/crud_application.png",
     description:
-      "This is a responsive GoDaddy-inspired landing page designed using Tailwind CSS",
-    view: "https://subham-web98.github.io/GoDaddy-UI/",
+      "A CRUD application built with the MERN stack, incorporating UI enhancements using React Toastify, SweetAlert, and Bootstrap.",
+    view: "https://crud-application-using-mern-stack.vercel.app/",
+  },
+
+  {
+    title: "Image Search App",
+    image: "/image_search_app.png",
+    description:
+      "An image search application developed with JavaScript, integrating the Unsplash API for image retrieval and utilizing Bootstrap for responsive layout and design.",
+    view: "https://subham-web98.github.io/Image-Search-App-Using-Rest-API/",
   },
 
   {
@@ -15,11 +23,25 @@ export const projects = [
     view: "https://subham-web98.github.io/Digital-Marketing-Course-/",
   },
   {
+    title: "GoDaddy UI Landing Page",
+    image: "/godaddy_ui.png",
+    description:
+      "This is a responsive GoDaddy-inspired landing page designed using Tailwind CSS",
+    view: "https://subham-web98.github.io/GoDaddy-UI/",
+  },
+  {
     title: "Company Website UI design",
     image: "/company_landing_page.png",
     description:
       "A fully responsive landing page designed for a company using Bootstrap.",
     view: "https://subham-web98.github.io/Nebula-bootstrap/",
+  },
+  {
+    title: "Password Generator",
+    image: "/password_generator.png",
+    description:
+      "A password generator application developed using HTML, CSS, and JavaScript.",
+    view: "https://subham-web98.github.io/Password_generator/",
   },
   {
     title: "Random Color Generator",
